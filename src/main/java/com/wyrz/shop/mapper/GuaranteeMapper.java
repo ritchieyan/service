eee81af1b0e148819feaf8c2a68dbcb431475a86
yanziqi <yanziq@caifu500.com>
2014-12-17 20:29:10 +0800

    修改mapper Signed-off-by: yanziqi <yanziq@caifu500.com>

--- src/main/java/com/wyrz/shop/mapper/GuaranteeMapper.java
+++ src/main/java/com/wyrz/shop/mapper/GuaranteeMapper.java
@@ -1,10 +1,12 @@
 package com.wyrz.shop.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectProvider;
+import org.springframework.data.domain.Pageable;
 
 import com.wyrz.core.mapper.BaseMapper;
 import com.wyrz.shop.domain.Guarantee;
@@ -26,7 +28,60 @@ public interface GuaranteeMapper extends BaseMapper<Guarantee> {
 	public <V extends Guarantee> V selectById(@Param("guaranteeId") Integer id);
 
 	@Override
-	@SelectProvider(method = "deleteAll", type = GuaranteeMapperProvider.class)
+	@Select("SELECT GUARANTEE_ID as guaranteeId FROM GUARANTEE")
+	public List<String> selectAllId();
+
+	@Override
+	@SelectProvider(method = "selectByIdList", type = GuaranteeMapperProvider.class)
+	public <V extends Guarantee> List<V> selectByIdList(List<Integer> idList);
+
+	@Override
+	@Select("SELECT COUNT(*) FROM GUARANTEE")
+	public Long selectCount();
+
+	@Override
+	@SelectProvider(method = "selectCount", type = GuaranteeMapperProvider.class)
+	public Long selectCount(Guarantee query);
+
+	@Override
+	@SelectProvider(method = "selectIdList", type = GuaranteeMapperProvider.class)
+	public List<String> selectIdList(Guarantee query);
+
+	@Override
+	public <V extends Guarantee> List<V> selectList(Guarantee query);
+
+	@Override
+	public <V extends Guarantee> List<V> selectList(Guarantee query, Pageable pageable);
+
+	@Override
+	public <K, V extends Guarantee> Map<K, V> selectMap(Guarantee query, String mapKey);
+
+	@Override
+	public <K, V extends Guarantee> Map<K, V> selectMap(Guarantee query, String mapKey, Pageable pageable);
+
+	@Override
+	public <V extends Guarantee> V selectOne(Guarantee query);
+
+	@Override
+	public int updateById(Guarantee entity);
+
+	@Override
+	public int updateByIdSelective(Guarantee entity);
+
+	@Override
+	public void insert(Guarantee entity);
+
+	@Override
+	public int delete(Guarantee query);
+
+	@Override
+	public int deleteById(Integer id);
+
+	@Override
+	public void deleteByIdInBatch(List<Integer> idList);
+
+	@Override
+	@Select("DELETE FROM GUARANTEE")
 	public int deleteAll();
 
 }
--- src/main/java/com/wyrz/shop/mapper/provider/GuaranteeMapperProvider.java
+++ src/main/java/com/wyrz/shop/mapper/provider/GuaranteeMapperProvider.java
@@ -1,5 +1,11 @@
 package com.wyrz.shop.mapper.provider;
 
+import java.util.List;
+
+import org.springframework.util.StringUtils;
+
+import com.wyrz.shop.domain.Guarantee;
+
 /**
  * 合作机构Sql支持器
  * @author Ritchieyan
@@ -7,9 +13,50 @@ package com.wyrz.shop.mapper.provider;
  */
 public class GuaranteeMapperProvider {
 
-	public String deleteAll() {
-		return "DELETE FORM GUARANTEE";
+	public String selectByIdList(List<Integer> idList) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("SELECT GUARANTEE_ID as guaranteeId, GUARANTEE_NAME as guaranteeName, GUARANTEE_TEL as guaranteeTel ");
+		sb.append("WHERE GUARANTEE_ID IN (");
+		for (Integer id : idList) {
+			sb.append(id);
+			if (idList.indexOf(id) > (idList.size() - 1)) {
+				sb.append(",");
+			}
+		}
+		sb.append(");");
+		return sb.toString();
+	}
 
+	public String selectCount(Guarantee query) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("SELECT COUNT(*) FROM GUARNATEE ");
+		sb.append("WHERE 1=1 ");
+		if (StringUtils.isEmpty(query.getGuaranteeId())) {
+			sb.append("AND GUARANTEE_ID=#{guaranteeId} ");
+		}
+		if (StringUtils.isEmpty(query.getGuaranteeName())) {
+			sb.append("AND GUARANTEE_NAME=#{guaranteeName} ");
+		}
+		if (StringUtils.isEmpty(query.getGuaranteeTel())) {
+			sb.append("AND GUARANTEE_TEL=#{guaranteeTel} ");
+		}
+		return sb.toString();
+	}
+
+	public String selectIdList(Guarantee query) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("SELECT GUARANTEE_ID ");
+		sb.append("WHERE 1=1 ");
+		if (StringUtils.isEmpty(query.getGuaranteeId())) {
+			sb.append("AND GUARANTEE_ID=#{guaranteeId} ");
+		}
+		if (StringUtils.isEmpty(query.getGuaranteeName())) {
+			sb.append("AND GUARANTEE_NAME=#{guaranteeName} ");
+		}
+		if (StringUtils.isEmpty(query.getGuaranteeTel())) {
+			sb.append("AND GUARANTEE_TEL=#{guaranteeTel} ");
+		}
+		return sb.toString();
 	}
 
 }
